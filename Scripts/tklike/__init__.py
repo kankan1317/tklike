046be9aba340ceafe535281b54485afe537bedcb
kankan1317 <87018974+kankan1317@users.noreply.github.com>
2023-03-14 19:55:52 +0900

    windowクラスから呼び出す基底クラスを定義

--- Scripts/tklike/__init__.py
+++ Scripts/tklike/__init__.py
@@ -3,6 +3,8 @@
 # importing tkinter module
 import tkinter as tk
 
+import _baseWindow
+
 __all__ = [
 
 ]
@@ -13,4 +15,10 @@ class window:
     ### class variable ###
     __is_firstWindow : bool = True
 
-    def __init__(self, title:str, width=None, height=None): ...
+    def __init__(self, title:str, width:int, height=int, x:int=0, y:int=0):
+        if window.__is_firstWindow == True:
+            self.window = _baseWindow.mainWindow(title, width, height, x, y)
+            window.__is_firstWindow = False
+        else:
+            self.window = _baseWindow.subWindow(title, width, height, x, y)
+            window.__is_firstWindow = False
