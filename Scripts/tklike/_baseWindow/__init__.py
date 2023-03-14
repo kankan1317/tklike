c8e3edfb7ab73ef9130a65c03503ccd72d687159
kankan1317 <87018974+kankan1317@users.noreply.github.com>
2023-03-14 18:29:04 +0900

    位置指定引数の設定

--- Scripts/tklike/_baseWindow/__init__.py
+++ Scripts/tklike/_baseWindow/__init__.py
@@ -3,16 +3,16 @@
 import tkinter as tk
 
 class mainWindow:
-    def __init__(self, title:str, width:int, height:int):
+    def __init__(self, title:str, width:int, height:int, x:int=0, y:int=0):
         self.master = tk.Tk()
 
         self.master.title(title)
-        self.master.geometry(f"{width}x{height}")
+        self.master.geometry(f"{width}x{height}+{x}+{y}")
 
 
 class subWindow:
-    def __init__(self, title:str, width:int, height:int):
+    def __init__(self, title:str, width:int, height:int, x:int=0, y:int=0):
         self.master = tk.Toplevel()
 
         self.master.title(title)
-        self.master.geometry(f"{width}x{height}")
+        self.master.geometry(f"{width}x{height}+{x}+{y}")
